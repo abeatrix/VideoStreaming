a7499d091c42b780afebaec90b6d6f12db257a41
Beatrix Woo <beatrixwho@gmail.com>
2020-12-14 14:00:23 -0800

    add readme. app base developed

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const fbConfig = {
+    apiKey: process.env.REACT_APP_FB_APIKey,
+    authDomain: process.env.REACT_APP_FB_AUTHD,
+    databaseURL: process.env.REACT_APP_FB_DBURL,
+    projectId: process.env.REACT_APP_FB_PID,
+    storageBucket: process.env.REACT_APP_FB_SB,
+    messagingSenderId: process.env.REACT_APP_FB_MSID,
+    appId: process.env.REACT_APP_FB_APPID,
+    measurementId: process.env.
+}
+
+export default fbConfig;
